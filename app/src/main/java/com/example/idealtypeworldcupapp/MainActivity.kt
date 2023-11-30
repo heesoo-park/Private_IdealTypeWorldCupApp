c60e70c5233db7ed11788b0e47f940d3fdb87af1
phs8505 <phs8505@gmail.com>
2023-11-30 19:29:25 +0900

    시작화면 #6 : MainActivity 라디오 그룹 내의 라디오버튼 클릭 리스너와 라디오버튼 선택값 SelectActivity로 넘기는 것 완료

--- app/src/main/java/com/example/idealtypeworldcupapp/MainActivity.kt
+++ app/src/main/java/com/example/idealtypeworldcupapp/MainActivity.kt
@@ -3,19 +3,34 @@ package com.example.idealtypeworldcupapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.idealtypeworldcupapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var round: Int? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.roundRadioGroup.setOnCheckedChangeListener { _, i ->
+            when (i) {
+                binding.firstRadioBtn.id -> round = 4
+                binding.secondRadioBtn.id -> round = 8
+                binding.thirdRadioBtn.id -> round = 16
+            }
+        }
         binding.startBtn.setOnClickListener {
-            val intent = Intent(this@MainActivity, SelectActivity::class.java)
-            startActivity(intent)
+            if (binding.roundRadioGroup.checkedRadioButtonId != -1) {
+                val intent = Intent(this@MainActivity, SelectActivity::class.java)
+                intent.putExtra("totalRound", round)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "라운드 수를 체크해주세요", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
