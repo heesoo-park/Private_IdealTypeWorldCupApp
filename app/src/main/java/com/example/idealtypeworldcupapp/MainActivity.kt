1694f4b314923a7e1831cd776159003e3b4d9643
phs8505 <phs8505@gmail.com>
2023-11-30 19:12:17 +0900

    시작화면 #5 #6 : MainActivity와 SelectActivity를 버튼 클릭 이벤트로 연결

--- app/src/main/java/com/example/idealtypeworldcupapp/MainActivity.kt
+++ app/src/main/java/com/example/idealtypeworldcupapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.idealtypeworldcupapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.idealtypeworldcupapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            val intent = Intent(this@MainActivity, SelectActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
