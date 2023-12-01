57ecee720c554e18a616d4b37d21624e44f9cc61
phs8505 <phs8505@gmail.com>
2023-12-01 17:38:58 +0900

    시작화면 : Log 라이브러리 제거

--- app/src/main/java/com/example/idealtypeworldcupapp/MainActivity.kt
+++ app/src/main/java/com/example/idealtypeworldcupapp/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.idealtypeworldcupapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import com.example.idealtypeworldcupapp.databinding.ActivityMainBinding
 
